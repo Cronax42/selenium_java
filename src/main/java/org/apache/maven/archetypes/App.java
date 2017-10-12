a78fe03edcf68e4d429683199dd2d52f988e3760
Sean van Basten Batenburg <svbasten@vxcompany.com>
2017-10-12 13:21:54 +0200

    First shot at using webdriver to open lmgtfy.com and performing search

--- src/main/java/org/apache/maven/archetypes/App.java
+++ src/main/java/org/apache/maven/archetypes/App.java
@@ -1,13 +1,27 @@
 package org.apache.maven.archetypes;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class testje {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("lmgtfy.com")
+
+        WebElement element = driver.findElement(By.cssSelector("div.search-term-input input"));
+
+        element.sendKeys("hello world Selenium");
+        element.submit();
+
+        pagetitle = driver.getTitle();
+
+        System.out.println(pagetitle);
+        
+        driver.quit();
     }
 }
