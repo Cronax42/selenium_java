e4d87c0f76b6c1f8ab14869721a91fea42dcfbff
Sean van Basten Batenburg <svbasten@vxcompany.com>
2017-10-12 13:31:12 +0200

    Small refactoring following IntelliJ advice

--- src/main/java/org/apache/maven/archetypes/App.java
+++ src/main/java/org/apache/maven/archetypes/App.java
@@ -4,24 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.support.ui.ExpectedCondition;
-//import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class testje {
+public class App {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
 
-        driver.get("lmgtfy.com")
+        driver.get("lmgtfy.com");
 
         WebElement element = driver.findElement(By.cssSelector("div.search-term-input input"));
 
         element.sendKeys("hello world Selenium");
         element.submit();
 
-        pagetitle = driver.getTitle();
+        String pageTitle = driver.getTitle();
 
-        System.out.println(pagetitle);
-        
+        System.out.println(pageTitle);
+
         driver.quit();
     }
 }
