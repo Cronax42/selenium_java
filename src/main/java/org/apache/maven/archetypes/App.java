872d87fb016fc21475f335b75247bb24882be31f
Sean van Basten Batenburg <svbasten@vxcompany.com>
2017-10-12 14:55:38 +0200

    Added assertion to check the generated URL

--- src/main/java/org/apache/maven/archetypes/App.java
+++ src/main/java/org/apache/maven/archetypes/App.java
@@ -21,10 +21,18 @@ public class App {
 
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver driver) {
-                driver.findElement(By.cssSelector("div.panel-success")).isDisplayed();
+                return driver.findElement(By.cssSelector("div.panel-success")).isDisplayed();
             }
         });
 
-        System.out.println("Koe");
+        String generatedUrl = driver.findElement(By.cssSelector("input[ng-model='link.url']")).getText();
+
+        //String expectedUrl = "http://lmgtfy.com/?q=hello+world+Selenium";
+
+        String expectedUrl = "bla";
+
+        assert generatedUrl.equals(expectedUrl);
+
+        driver.quit();
     }
 }
